b0df599b52f3f3d341706b2b7919e1eaa0685a80
craterface <craterface77@gmail.com>
2022-04-17 19:16:35 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Scrip for deploying Metadata to IPFS
+
+# Run:
+
+```
+node ./ipfs-script.js
+```
--- ipfs.js
+++
@@ -1,7 +0,0 @@
-import { create } from "ipfs-http-client";
-
-export const ipfs = create({
-  host: "ipfs.infura.io",
-  port: 5001,
-  protocol: "https",
-});
